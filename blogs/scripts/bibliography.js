0e7d9261b5a2fe143ec2bf34f90a1c4bd603004b
Patrick Virie <p.virie@gmail.com>
2023-08-14 22:53:58 +0700

    update bib system, adjust abstract

--- blogs/scripts/bibliography.js
+++ blogs/scripts/bibliography.js
@@ -1,3 +1,3 @@
-const bib_index={};const compile_citation=function(){const bib=document.querySelector(".bibliography");for(const b of citation_blocks){const content=b.innerHTML;if(bib_index[content]==null){bib_index[content]=Object.keys(bib_index).length+1;}
-if(bib!=null){const li=document.createElement("li");li.innerHTML=bib_index[content]+". "+content;bib.appendChild(li);}
-b.innerHTML=bib_index[content];}};const citation_blocks=document.querySelectorAll(".cite");window.addEventListener("load",function(){compile_citation();});
+const bib_index={};const compile_citation=function(){const bib=document.querySelector(".bibliography");let index=0;for(const b of citation_blocks){const content=b.innerHTML;if(bib_index[content]==null){bib_index[content]=Object.keys(bib_index).length+1;}
+if(bib!=null){const li=document.createElement("li");li.innerHTML=bib_index[content]+". "+content;li.id=`cite-${index}`;bib.appendChild(li);b.addEventListener("click",function(){li.scrollIntoView();});}
+b.innerHTML=bib_index[content];index+=1;}};const citation_blocks=document.querySelectorAll(".cite");window.addEventListener("load",function(){compile_citation();});
