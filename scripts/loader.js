d1e6402d093da0ac5ead9d2cc2abb73ef54f0950
Chatavut Viriyasuthee <p.virie@gmail.com>
2024-06-04 21:30:33 +0700

    update loader

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,4 +1,5 @@
-const display_load=function(flag){if(window.loader_dom==null){const loader_dom=document.createElement("div");loader_dom.classList.add("float-loader","hide");loader_dom.innerHTML=`
-            <div class="loader"><span></span></div>
+const display_load=function(flag){if(window.loader_dom==null){const loader_dom=document.createElement("dialog");loader_dom.classList.add("float-loader","hide");loader_dom.innerHTML=`
+            <span class="hide">loading</span>
+            <span class="loader"></span>
         `;document.body.appendChild(loader_dom);window.loader_dom=loader_dom;}
-if(flag){window.loader_dom.classList.remove("hide");}else{window.loader_dom.classList.add("fade-out");setTimeout(()=>{window.loader_dom.classList.add("hide");},1000);}};display_load(true);
+if(flag){window.loader_dom.classList.remove("hide");window.loader_dom.classList.remove("fade-out");window.loader_dom.showModal();}else{window.loader_dom.classList.add("fade-out");setTimeout(()=>{window.loader_dom.classList.add("hide");window.loader_dom.close();},250);}};display_load(true);
