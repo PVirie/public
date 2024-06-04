2029af16b2b334fcd73586861e414095a9d67988
Chatavut Viriyasuthee <p.virie@gmail.com>
2024-06-04 16:37:21 +0700

    update loader

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,1 +1,4 @@
-const display_load=function(flag){const loading=document.querySelector("#loader");if(flag){loading.classList.remove("hide");}else{loading.classList.add("fade-out");setTimeout(()=>{loading.classList.add("hide");},1000);}};
+const display_load=function(flag){if(window.loader_dom==null){const loader_dom=document.createElement("div");loader_dom.classList.add("float-loader","hide");loader_dom.innerHTML=`
+            <div class="loader"><span></span></div>
+        `;document.body.appendChild(loader_dom);window.loader_dom=loader_dom;}
+if(flag){window.loader_dom.classList.remove("hide");}else{window.loader_dom.classList.add("fade-out");setTimeout(()=>{window.loader_dom.classList.add("hide");},1000);}};display_load(true);
